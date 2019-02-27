c6c4a4dd177f12ac90a87b27f2f6a73a74ff35de
Richard Ali <richardali@richards-mbp.lan>
2019-02-26 23:57:03 -0800

    update with auth

--- src/js/components/tasker.js
+++ src/js/components/tasker.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import AWS from 'aws-sdk';
-import {Amplify, Auth} from 'aws-amplify';
 import logo from '../../asset/image/logo.svg';
 import '../../asset/style.css';
-import { Authenticator, Greetings } from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+import { Authenticator } from 'aws-amplify-react';
 
-// Amplify.configure({
-//   Auth: {
-//       identityPoolId: 'us-west-2', // Amazon Cognito Identity Pool ID
-//       region: 'us-west-2', // Amazon Cognito Region
-//   }
-// });
+Amplify.configure({
+    Auth: {
+        // REQUIRED - Amazon Cognito Identity Pool ID
+        identityPoolId: 'us-west-2:6a3e316e-4499-457d-a987-3c09f6bba521', 
+        // REQUIRED - Amazon Cognito Region
+        region: 'us-west-2', 
+        // OPTIONAL - Amazon Cognito User Pool ID
+        userPoolId: 'us-west-2_LdXXPDqxv',
+        // OPTIONAL - Amazon Cognito Web Client ID
+        userPoolWebClientId: '31vrsnu30qa0rpg7qlofb932cn', 
+    }
+});
 
 @observer class Tasker extends React.Component {
   render () {
     return (
         <div className="App">
+        <Authenticator>
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
@@ -30,8 +37,8 @@ import { Authenticator, Greetings } from 'aws-amplify-react';
             >
               Learn React
             </a>
-            
           </header>
+          </Authenticator>
         </div>
     );
   }
